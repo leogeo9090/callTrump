af3db1fb6496aca60da7bd984684dd616df8ca4f
Alexander Teinum <ateinum@gmail.com>
2014-10-11 14:10:40 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# simple-website
+
+## License
+
+BSD
---
+++ main.go
@@ -0,0 +1,292 @@
+package main
+
+import (
+	"os"
+	"io/ioutil"
+	"bytes"
+	"strings"
+	"time"
+	"github.com/russross/blackfriday"
+)
+
+func siteTitle() string {
+	return strings.Split(string(getFile("header.md")), "\n")[0][2:]
+}
+
+func getHeader(title string) string {
+	return `<html>
+		<head>
+			<meta charset="utf-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1">
+			<title>` + title + `</title>
+			<style>
+				#page {
+					margin: 2.5em auto;
+					max-width: 30em;
+					padding: 0 0.5rem;
+				}
+
+				a {
+					text-decoration: none;
+					color: #03c;
+				}
+
+				a:visited {
+					color: #639;
+				}
+
+				a:hover {
+					text-decoration: underline;
+				}
+
+				ul {
+					list-style-type: none;
+					padding: 0;
+				}
+
+				html {
+					font-size: 1em;
+				}
+
+				body {
+				  background-color: #fff;
+				  color: #333;
+				  font-weight: 400;
+				  line-height: 1.45;
+				  font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+				  text-rendering: optimizeLegibility;
+				}
+
+				p {
+					margin-bottom: 1.3em;
+				}
+
+				h1, h2, h3, h4 {
+				  margin: 1.414em 0 0.5em;
+				  font-weight: 600;
+				  line-height: 1.2;
+				}
+
+				h1 {
+				  margin-top: 0;
+				  font-size: 1.602em;
+				}
+
+				h2 {
+					font-size: 1.424em;
+				}
+
+				h3 {
+					font-size: 1.266em;
+				}
+
+				h4 {
+					font-size: 1.125em;
+				}
+
+				pre {
+					background-color: #eee;
+				}
+			</style>
+		</head>
+		<body>
+			<div id="page">`
+}
+
+func getFile(f string) []byte {
+	b, err := ioutil.ReadFile(f)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return b
+}
+
+func getDir(dir string) []os.FileInfo {
+	p, err := ioutil.ReadDir(dir)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return p
+}
+
+func GetPostMeta(fi os.FileInfo) (string, string, string) {
+	id := fi.Name()[:len(fi.Name()) - 3]
+	date := fi.Name()[0:10]
+	title := strings.Split(string(getFile("posts/" + fi.Name())), "\n")[0][2:]
+
+	return id, date, title
+}
+
+func GetPageMeta(fi os.FileInfo) (string, string) {
+	id := fi.Name()[:len(fi.Name()) - 3]
+	title := strings.Split(string(getFile("pages/" + fi.Name())), "\n")[0][2:]
+
+	return id, title
+}
+
+func writeFile(fileName string, b bytes.Buffer) {
+	err := ioutil.WriteFile("out/" + fileName + ".html", b.Bytes(), 0644)
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func writeLayout(b *bytes.Buffer, title string) {
+	b.WriteString(getHeader(title))
+}
+
+func writePostsSection(b *bytes.Buffer) {
+	b.WriteString("<h2>Posts</h2><ul>")
+
+	posts := getDir("posts")
+
+	for i := len(posts) - 1; i >= 0; i-- {
+		fileName, date, title := GetPostMeta(posts[i])
+
+		b.WriteString("<li><a href=\"posts/" +
+			fileName + ".html\">" +
+			date + " – " +
+			title + "</a></li>\n")
+	}
+
+	b.WriteString("</ul><p><a href=\"all-posts.html\">All posts</a></p>")
+}
+
+func writePagesSection(b *bytes.Buffer) {
+	b.WriteString("<h2>Pages</h2><ul>")
+
+	pages := getDir("pages")
+
+	for i := 0; i < len(pages); i++ {
+		id, title := GetPageMeta(pages[i])
+
+		b.WriteString("<li><a href=\"pages/" +
+			id + ".html\">" +
+			title + "</a></li>\n")
+	}
+
+	b.WriteString("</ul>")
+}
+
+func writePosts() {
+	posts := getDir("posts")
+
+	for i := 0; i < len(posts); i++ {
+		var b bytes.Buffer
+
+		id, date, title := GetPostMeta(posts[i])
+
+		writeLayout(&b, title + " – " + siteTitle())
+		b.WriteString("<p><a href=\"../index.html\">←</a></p>")
+		b.WriteString("<p class=\"font_small\">" + date + "</p>")
+		b.Write(blackfriday.MarkdownBasic(getFile("posts/" + posts[i].Name())))
+		b.WriteString("<p><a href=\"../index.html\">←</a></p></div></body></html>")
+
+		writeFile("posts/" + id, b)
+	}
+}
+
+func writePostsPage() {
+	posts := getDir("posts")
+	var b bytes.Buffer
+
+	writeLayout(&b, "All posts – " + siteTitle())
+	b.WriteString("<p><a href=\"index.html\">←</a></p>")
+	b.WriteString("<h1>All posts</h1>")
+	b.WriteString("<ul>")
+
+	for i := 0; i < len(posts); i++ {
+
+		id, date, title := GetPostMeta(posts[i])
+
+		b.WriteString("<li><a href=\"posts/" +
+			id + ".html\">" +
+			date + " – " +
+			title + "</a></li>\n")
+
+	}
+
+	b.WriteString("</ul><p><a href=\"index.html\">←</a></p>")
+	b.WriteString("</div></body></html>")
+	writeFile("all-posts", b)
+}
+
+func writePages() {
+	pages := getDir("pages")
+
+	for i := 0; i < len(pages); i++ {
+		var b bytes.Buffer
+
+		fileName, title := GetPageMeta(pages[i])
+
+		writeLayout(&b, title + " – " + siteTitle())
+		b.WriteString("<p><a href=\"../index.html\">←</a></p>")
+		b.Write(blackfriday.MarkdownBasic(getFile("pages/" + pages[i].Name())))
+		b.WriteString("<p><a href=\"../index.html\">←</a></p></div></body></html>")
+
+		writeFile("pages/" + fileName, b)
+	}
+}
+
+func createFilesAndDirs() {
+	if _, err := os.Stat("header.md"); os.IsNotExist(err) {
+		err := ioutil.WriteFile(
+			"header.md",
+			[]byte("# Title\n\nDescription"),
+			0644)
+
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	if _, err := os.Stat("posts"); os.IsNotExist(err) {
+		os.MkdirAll("posts", 0755)
+
+		err := ioutil.WriteFile(
+			"posts/" + time.Now().Format("2006-01-02") + "-initial-post.md",
+			[]byte("# Initial post\n\nThis is the initial post."),
+			0644)
+
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	if _, err := os.Stat("pages"); os.IsNotExist(err) {
+		os.MkdirAll("pages", 0755)
+
+		err := ioutil.WriteFile(
+			"pages/about.md",
+			[]byte("# About\n\nThis is the about page."),
+			0644)
+
+		if err != nil {
+			panic(err)
+		}
+	}
+	os.MkdirAll("out", 0755)
+	os.MkdirAll("out/posts", 0755)
+	os.MkdirAll("out/pages", 0755)
+}
+
+func main() {
+	createFilesAndDirs()
+
+	var b bytes.Buffer
+	writeLayout(&b, siteTitle())
+	b.Write(blackfriday.MarkdownBasic(getFile("header.md")))
+	writePostsSection(&b)
+	writePagesSection(&b)
+	b.WriteString("</div></body></html>")
+	writeFile("index", b)
+
+	writePosts()
+	writePages()
+	writePostsPage()
+}
